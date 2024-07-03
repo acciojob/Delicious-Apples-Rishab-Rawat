1f25ca3841449d9d071341093e2ba22cb0041e0e
Rishab-Rawat <rawatrishab66@gmail.com>
2024-07-03 20:11:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,1 @@
 //your code here
-describe('Apple Varieties Page', () => {
-    it('should display "ADD TO CART" button', () => {
-        cy.visit('path/to/your/page');
-        cy.contains('button', 'ADD TO CART').should('be.visible');
-    });
-
-    it('should display "BENEFITS" section', () => {
-        cy.visit('path/to/your/page');
-        cy.contains('h2', 'BENEFITS').should('be.visible');
-    });
-});
